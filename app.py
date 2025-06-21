ec97148991a699cdf5c5bc22049577b24470f19f
rahmidwintan <raher2159@gmail.com>
2025-06-22 01:58:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def about_page():
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.image("C:\Rahmi\kuliah\SEMESTER 6\PI Rahmi\DATASET\d5300022417f98c2766d3b6b1282.png", caption="Matang", use_container_width=True)
+        st.image("https://drive.google.com/file/d/1zho1OX-XdeZVSNWGEQzPkiYmaM61j3Xw/view?usp=drive_link", caption="Matang", use_container_width=True)
         st.markdown("""
         **Matang (Grade A)**  
         - Warna merah merata  
@@ -127,7 +127,7 @@ def about_page():
         """)
 
     with col2:
-        st.image("C:\Rahmi\kuliah\SEMESTER 6\PI Rahmi\DATASET\879d5fea6405226d84f43f2bfa8d1f4b.png", caption="Setengah Matang", use_container_width=True)
+        st.image("https://drive.google.com/file/d/1A5OzVn_g6jfEvI5dfEb1ARh9PZRxpC8Q/view?usp=drive_link", caption="Setengah Matang", use_container_width=True)
         st.markdown("""
         **Setengah Matang (Grade B)**  
         - Warna merah-kuning  
@@ -136,7 +136,7 @@ def about_page():
         """)
 
     with col3:
-        st.image("C:\Rahmi\kuliah\SEMESTER 6\PI Rahmi\DATASET\9c3bc2101bacfee1f48aaa76fcd7bc4b.png", caption="Mentah", use_container_width=True)
+        st.image("https://drive.google.com/file/d/1xqRA3cOA_53E1voaxQ_rnT8RGI3HE4Hu/view?usp=drive_link", caption="Mentah", use_container_width=True)
         st.markdown("""
         **Mentah (Grade C)**  
         - Warna hijau mendominasi  
